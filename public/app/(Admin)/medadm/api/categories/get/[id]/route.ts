0fce54bd4ac6a7a98728a2d79b67497ffd68457b
tontanagon <tontanagon555@gmail.com>
2026-01-08 10:40:07 +0700

    add cate api

---
+++ public/app/(Admin)/medadm/api/categories/get/[id]/route.ts
@@ -0,0 +1,20 @@
+import { auth } from "@/auth";
+import { fetchWrapper } from "@/lib/fetchwrapper";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest,{ params }: { params: Promise<{ id: string }> }) {
+    const session = await auth();
+    const { id } = await params;
+
+    
+
+    const res = await fetchWrapper(`${process.env.API_BASE_URL}/api/categories/get/${id}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json", Authorization: `Bearer ${session?.user.token}` },
+    });
+
+    const data = await res.json();
+
+    return NextResponse.json(data, { status: res.status })
+
+}
